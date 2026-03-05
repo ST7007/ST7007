1280c678a72c4f42683b3f966bef71512e5a0547
Kelvin <yourgithubemail@example.com>
2026-03-06 00:43:41 +0800

    Update luxury homepage

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,20 @@ const urlsToCache = [
   "/css/style.css",
   "/js/script.js"  // replace with your JS file if different
 ];
+const CACHE_NAME = 'luxride-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/customer-enquiry.html',
+  '/css/style.css',
+  '/icons/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
 
 self.addEventListener("install", event => {
   event.waitUntil(
